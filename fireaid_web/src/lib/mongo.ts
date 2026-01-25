729b6d62b0f33d8d7f3b49254aa1b8859aa6c2ea
Jenae Matson <jlmatson@alaska.edu>
2026-01-25 14:40:43 -0900

    Add interface that sends term/definition to MongoDB

---
+++ fireaid_web/src/lib/mongo.ts
@@ -0,0 +1,37 @@
+import { MongoClient, Db } from "mongodb";
+
+// if (!process.env.MONGODB_URI) {
+//   throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
+// }
+
+const uri = "mongodb://root:password@mongo:27017/fireaid?authSource=admin";
+const options = { appName: "fireaid-nextjs" };
+
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
+
+if (process.env.NODE_ENV === "development") {
+  // In development mode, use a global variable so that the value
+  // is preserved across module reloads caused by HMR (Hot Module Replacement).
+  let globalWithMongo = global as typeof globalThis & {
+    _mongoClientPromise?: Promise<MongoClient>;
+  };
+
+  if (!globalWithMongo._mongoClientPromise) {
+    client = new MongoClient(uri, options);
+    globalWithMongo._mongoClientPromise = client.connect();
+  }
+
+  clientPromise = globalWithMongo._mongoClientPromise;
+} else {
+  // In production mode, it's best to not use a global variable.
+  client = new MongoClient(uri, options);
+  clientPromise = client.connect();
+}
+
+// Export a module-scoped MongoClient. By doing this in a
+// separate module, the client can be shared across functions.
+export async function getDb(): Promise<Db> {
+    const client = await clientPromise;
+    return client.db("fireaid");
+}
