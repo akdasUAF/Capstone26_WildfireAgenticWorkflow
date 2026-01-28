71c3737af16daf582545ca6ce413c0695b3c9079
Elliott Lewandowski <erlewandowski@alaska.edu>
2026-01-28 10:33:13 -0900

    Implementing .env file to store API key

--- fireaid_web/src/pages/api/ai/query.ts
+++ fireaid_web/src/pages/api/ai/query.ts
@@ -4,5 +4,10 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
+    if(!process.env.API_KEY) {
+        res.status(500).json({error: "Missing API key"})
+    }
+
+    
     res.status(200).json({msg: "This is an AI response!"})
 }
