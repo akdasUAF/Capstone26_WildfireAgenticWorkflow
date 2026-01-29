3039f9359cb80b776df838e83ef64e56443b768f
Elliott Lewandowski <erlewandowski@alaska.edu>
2026-01-29 10:24:50 -0900

    Removed debug sleep code

--- fireaid_web/src/pages/api/ai/query.ts
+++ fireaid_web/src/pages/api/ai/query.ts
@@ -1,22 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { GoogleGenAI } from "@google/genai";
-import { AIResponse } from "@/types/LLMPrompt.d";
-
-
 
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
-    /*
-    // FOR DEBUGGING: Use to simulate delayed response from endpoint without using up api calls
-    const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
-
-    await sleep(1000)
-
-    res.status(200).json({msg: "This is demo text to fill up some space so that we can make sure formatting is working properly"})
-    */
-
     if(!process.env.REACT_APP_API_KEY) {
         res.status(500).json({error: "Missing API key"})
     }
