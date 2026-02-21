d2d47b1b788841827e38381b606f2cd5e65901e5
Elliott Lewandowski <erlewandowski@alaska.edu>
2026-02-20 16:46:11 -0900

    Created Demo MCP Server

---
+++ mcp_server/main.py
@@ -0,0 +1,19 @@
+from typing import Any
+import os
+import httpx
+import logging
+from mcp.server.fastmcp import FastMCP
+from pymongo import MongoClient
+
+# Initialize FastMCP server
+mcp = FastMCP("terms")
+
+@mcp.tool()
+async def get_definition(term: str) -> str:
+    """Get term definition from database.
+
+    Args:
+        term: Term to get definition for
+    """
+
+    return "The tool has succeeded, fire is a block of ice"
